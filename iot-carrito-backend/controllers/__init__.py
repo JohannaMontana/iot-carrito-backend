a81841d69e95226716af358569f54bf0e4e8038e
Johanna Morales Mateos <123125719+JohannaMontana@users.noreply.github.com>
2025-10-28 16:13:01 -0600

    Delete iot-carrito-backend directory

--- iot-carrito-backend/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .carrito_controller import CarritoController
-from .monitoreo_controller import MonitoreoController
-from .obstaculos_controller import ObstaculosController
-
-__all__ = ['CarritoController', 'MonitoreoController', 'ObstaculosController']
--- iot-carrito-backend/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .db_connection import db
-from .models import *
-
-__all__ = ['db', 'Dispositivo', 'Movimiento', 'Obstaculo', 'SecuenciaDemo', 'EjecucionSecuencia']
