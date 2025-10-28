fe4870dfc03b9252b66429e09e7b65415b1fa9c1
Johanna Morales Mateos <123125719+JohannaMontana@users.noreply.github.com>
2025-10-28 16:13:32 -0600

    Add files via upload

---
+++ controllers/__init__.py
@@ -0,0 +1,5 @@
+from .carrito_controller import CarritoController
+from .monitoreo_controller import MonitoreoController
+from .obstaculos_controller import ObstaculosController
+
+__all__ = ['CarritoController', 'MonitoreoController', 'ObstaculosController']
---
+++ database/__init__.py
@@ -0,0 +1,4 @@
+from .db_connection import db
+from .models import *
+
+__all__ = ['db', 'Dispositivo', 'Movimiento', 'Obstaculo', 'SecuenciaDemo', 'EjecucionSecuencia']
